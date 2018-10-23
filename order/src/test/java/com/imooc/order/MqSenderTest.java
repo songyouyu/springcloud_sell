c04973032e4bd3e5a0481f364f648b475b4969ac
songyouyu <songyy950208@163.com>
2018-10-23 08:49:00 +0800

    统一配置中心

---
+++ order/src/test/java/com/imooc/order/MqSenderTest.java
@@ -0,0 +1,28 @@
+package com.imooc.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+/**
+ * @author youyusong
+ * @date 2018/10/22
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqSenderTest {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    public void send() {
+        amqpTemplate.convertAndSend("myQueue", "now : " + new Date());
+    }
+
+}
